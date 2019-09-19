fe42f4e01dac2546592b04d2065bc14b47d029d6
mcgithu <529672848@qq.com>
2019-09-19 10:09:34 +0800

    提交类

--- src/main/java/com/example/gitest/controller/UserController.java
+++ src/main/java/com/example/gitest/controller/UserController.java
@@ -1,4 +1,12 @@
 package com.example.gitest.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Slf4j
 public class UserController {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
 }
